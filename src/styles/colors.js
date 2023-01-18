94e0c1ba2ee4a78244530d6951f954f7089edd79
Noudea <oudea.nicolas@gmail.com>
2023-01-18 12:11:08 +0100

    :lipstick: feature(design-system): added colors

---
+++ src/styles/colors.js
@@ -0,0 +1,33 @@
+import {StyleSheet} from 'react-native';
+
+const Colors = StyleSheet.create({
+  primary: {
+    color: '#FF0659',
+  },
+  secondary1: {
+    color: '#15B4B2',
+  },
+  secondary2: {
+    color: '#FFC406',
+  },
+  secondary3: {
+    color: '#F89400',
+  },
+  secondary4: {
+    color: '#45C478',
+  },
+  secondary5: {
+    color: '#2B5DE4',
+  },
+  error: {
+    color: '#B00020',
+  },
+  white: {
+    color: '#FFFFFF',
+  },
+  black: {
+    color: '#000000',
+  },
+});
+
+export default Colors;
