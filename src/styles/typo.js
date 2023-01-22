015e066bd3396377dda503ff1ec7bc843c4753b4
Noudea <oudea.nicolas@gmail.com>
2023-01-22 18:14:30 +0100

    :sparkles: feat(design-system): Typo and Colors

---
+++ src/styles/typo.js
@@ -0,0 +1,36 @@
+import {StyleSheet} from 'react-native';
+
+const Typo = StyleSheet.create({
+  screenTitle: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#000',
+  },
+  sectionTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#000',
+  },
+  subSectionTitle: {
+    fontSize: 17,
+    fontWeight: '500',
+    color: '#000',
+  },
+  text: {
+    fontSize: 16,
+    fontWeight: '500',
+    color: '#000',
+  },
+  infos: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    color: '#000',
+  },
+  subInfos: {
+    fontSize: 13,
+    fontWeight: 'bold',
+    color: '#000',
+  },
+});
+
+export default Typo;
